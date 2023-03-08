eeb39811bebb378e7d6dc628b882b0438dd74352
grandpariley <rileyherman@tuta.io>
2023-03-07 20:55:51 -0600

    whoops

--- src/app/survey-container/survey-container.component.ts
+++ src/app/survey-container/survey-container.component.ts
@@ -18,7 +18,7 @@ import { SurveyService } from '../survey.service';
   </div>`,
 })
 export class SurveyContainerComponent implements OnInit {
-  state: FormControl<SurveyState> = new FormControl<SurveyState>('LONG', Validators.required);
+  state: FormControl<SurveyState> = new FormControl<SurveyState>('RISK', Validators.required);
 
   constructor(private surveyService: SurveyService, private router: Router) { }
 
